4588593862cda5c3390478470a2d9cd70599c94c
ningia <113545072+ningia92@users.noreply.github.com>
2025-04-24 23:57:51 +0200

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  const itemsToRemove = args;
+
+  return array.filter(item => !itemsToRemove.includes(item));
 };
 
 // Do not edit below this line
