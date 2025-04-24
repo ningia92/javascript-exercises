b83ddc687951e13dd68b85d23f594ae7b7930ab1
ningia <113545072+ningia92@users.noreply.github.com>
2025-04-25 00:20:28 +0200

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  if (num1 % 1 !== 0 || num2 % 1 !== 0) return 'ERROR';
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
+  
+  let sum = 0;
+  let first = num1 < num2 ? num1 : num2;
+  let last = num1 > num2 ? num1 : num2;
 
+  for (let i = first; i <= last; i++) sum += i;
+
+  return sum;
 };
 
 // Do not edit below this line
