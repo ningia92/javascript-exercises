ddcdff20e45b7b7bcacd3cf49e0984c198448c36
ningia <113545072+ningia92@users.noreply.github.com>
2025-04-28 23:16:48 +0200

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return 'OOPS'
+  else {
+    const sequence = [0, 1]
+    for (let i = 0; i < num; i++) {
+      let next = sequence[i] + sequence[i + 1]
+      sequence.push(next)
+    }
+    return sequence[num]
+  }
 };
 
 // Do not edit below this line
