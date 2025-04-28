d0c47e50857b9e2472f75061b22eb22dba4f4b6e
ningia <113545072+ningia92@users.noreply.github.com>
2025-04-29 00:21:20 +0200

    Complete exercise 12_findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people
+    .map(person => {
+      return !person.yearOfDeath
+        ? { name: person.name, age: new Date().getFullYear() - person.yearOfBirth }
+        : { name: person.name, age: person.yearOfDeath - person.yearOfBirth }
+    })
+    .reduce((acc, curr) => {
+      if (!acc || acc.age < curr.age) return curr
+      return acc
+    }, null)
 
+  return oldest
 };
 
 // Do not edit below this line
